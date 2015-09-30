afaa0e13b1fc857b55f9ace8258a0c1f5f07af5e
Sibin <sibinbk@gmail.com>
2015-09-30 23:50:06 +1000

    Navigation bar customisation.

--- Focus8/FLTimerViewController.m
+++ Focus8/FLTimerViewController.m
@@ -155,10 +155,19 @@ static NSString *const kFLAppTitle = @"Listie";
     // Set TimerView Interface
     [self setUpTimerViewInterfaceWith:self.isFullView];
     
+    // Make navigation bar transparent and set custom font for title.
     [[self.navigationController navigationBar] setBackgroundImage:[UIImage new]
                              forBarMetrics:UIBarMetricsDefault];
     [self.navigationController navigationBar].shadowImage = [UIImage new];
     [self.navigationController navigationBar].translucent = YES;
+    
+    NSShadow *shadow = [[NSShadow alloc] init];
+    shadow.shadowColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8];
+    shadow.shadowOffset = CGSizeMake(0, 1);
+    [self.navigationController.navigationBar setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
+                                                                      [UIColor colorWithRed:245.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0], NSForegroundColorAttributeName,
+                                                                      shadow, NSShadowAttributeName,
+                                                                      [UIFont fontWithName:@"HelveticaNeue-CondensedBlack" size:28.0], NSFontAttributeName, nil]];
     
     // Setting Summary view blur effect.
     self.summaryView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.2];
