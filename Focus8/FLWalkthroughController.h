5d7691d5270b64a67932c0af3cccb41605770e46
Sibin <sibinbk@gmail.com>
2015-10-08 06:26:10 +1100

    Temp commit

---
+++ Focus8/FLWalkthroughController.h
@@ -0,0 +1,14 @@
+//
+//  FLWalkthroughController.h
+//  Focus8
+//
+//  Created by Sibin Baby on 8/10/2015.
+//  Copyright © 2015 FocusApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <EAIntroView/EAIntroView.h>
+
+@interface FLWalkthroughController : UIViewController <EAIntroDelegate>
+
+@end
