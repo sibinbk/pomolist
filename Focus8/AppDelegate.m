288999f0abb9150f8411e6d50e61f5d2efd58b06
Sibin <sibinbk@gmail.com>
2015-10-07 21:54:50 +1100

    App Delegate cleaned.

--- Focus8/AppDelegate.m
+++ Focus8/AppDelegate.m
@@ -96,10 +96,13 @@
         dict[NSLocalizedFailureReasonErrorKey] = failureReason;
         dict[NSUnderlyingErrorKey] = error;
         error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
-        // Replace this with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        abort();
+
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"Error : %@, %@", error, [error userInfo]]
+                                                        message:@"Something went wrong. Restarting app might fix it."
+                                                       delegate:self
+                                              cancelButtonTitle:@"Ok"
+                                              otherButtonTitles:nil];
+        [alert show];
     }
     
     return _persistentStoreCoordinator;
@@ -128,10 +131,12 @@
     if (managedObjectContext != nil) {
         NSError *error = nil;
         if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
-            // Replace this implementation with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-            abort();
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Error", @"Error")
+                                                            message:NSLocalizedString(@"Error in saving data", @"Error in saving data")
+                                                           delegate:self
+                                                  cancelButtonTitle:NSLocalizedString(@"Ok", @"Ok")
+                                                  otherButtonTitles:nil];
+            [alert show];
         }
     }
 }
